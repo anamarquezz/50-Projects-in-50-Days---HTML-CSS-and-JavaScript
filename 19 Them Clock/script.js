1b9f6a4bcc11bd47976c50de7909dbabe1933ea9
AnaMarquez <anamarquez.edu@hotmail.com>
2022-02-15 23:40:16 -0800

    Theme Clock

---
+++ 19 Them Clock/script.js
@@ -0,0 +1,6 @@
+const hourEl = document.querySelector('.hour')
+const minuteEl = document.querySelector('.minute')
+const secondEl = document.querySelector('.second')
+const timeEl = document.querySelector('.timeEl')
+const dateEl = document.querySelector('.dateEl')
+const toggle = document.querySelector('.toggle')
