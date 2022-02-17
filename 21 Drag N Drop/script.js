b626dbd7f96d1ef408248553b7857d7b8c7a2dfe
AnaMarquez <anamarquez.edu@hotmail.com>
2022-02-16 22:40:52 -0800

    Drag N Drop

---
+++ 21 Drag N Drop/script.js
@@ -0,0 +1,48 @@
+const fill = document.querySelector('.fill')
+const empties = document.querySelectorAll('.empty')
+
+fill.addEventListener('dragstart', dragStart)
+fill.addEventListener('dragend', dragEnd)
+
+for(const empty of empties) {
+    empty.addEventListener('dragover', dragOver)
+    empty.addEventListener('dragenter', dragEnter)
+    empty.addEventListener('dragleave', dragLeave)
+    empty.addEventListener('drop', dragDrop)
+}
+
+function dragStart() {
+    this.className += ' hold' 
+    setTimeout(() => this.className = 'invisible', 0)
+}
+
+function dragEnd() {
+    this.className = 'fill'
+}
+
+
+function dragOver(e) {
+    e.preventDefault()
+}
+
+function dragEnter(e) {
+    e.preventDefault()
+    this.className += ' hovered'
+}
+
+function dragEnter(e) {
+    e.preventDefault()
+    this.className += ' hovered'
+
+}
+
+
+function dragLeave() {
+    this.className = 'empty'
+    console.log('drag Leave')
+}
+
+function dragDrop() {
+    this.className = 'empty'
+    this.append(fill)
+}
