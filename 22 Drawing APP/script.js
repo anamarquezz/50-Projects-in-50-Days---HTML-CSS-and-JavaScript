7bd5f8c5d626eade7c292436406c5bab9e1e5ce2
AnaMarquez <anamarquez.edu@hotmail.com>
2022-02-18 22:05:30 -0800

    drawing APP

--- 22 Drawing APP/script.js
+++ 22 Drawing APP/script.js
@@ -1,8 +1,15 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
+const increaseBtn = document.getElementById('increase');
+const decreaseBtn = document.getElementById('decrease');
+const sizeEl = document.getElementById('size');
+const colorEl = document.getElementById('color');
+const clearEl = document.getElementById('clear');
 
-let size = 20
+
+
+let size = 10
 let isPressed = false
 let color = 'black'
 let x  
@@ -14,8 +21,30 @@ canvas.addEventListener('mousedown', (e) =>{
     x = e.offseetX
     y = e.offseetY
 
-    console.log(isPressed,x , y)
-})
+    
+});
+
+canvas.addEventListener('mouseup', (e) =>{
+    isPressed = false
+
+    x = undefined
+    y = undefined
+
+   
+});
+
+canvas.addEventListener('mousemove', (e) =>{
+  if(isPressed) {
+      const x2 = e.offsetX
+      const y2 = e.offsetY
+      
+      drawCircle(x2,y2)
+      drawLine(x,y, x2, y2)
+      x = x2
+      y =y2
+  }   
+});
+
 function drawCircle(x, y){
     ctx.beginPath();
     ctx.arc(x, y, size, 0, Math.PI * 2, true);
@@ -28,10 +57,35 @@ function drawLine(x1, y1, x2, y2) {
     ctx.moveTo(x1, y1);
     ctx.lineTo(x2,y2);
     ctx.strokeStyle = color;
-    ctx.lineWidth = size;
+    ctx.lineWidth = size * 2;
     ctx.stroke()
 }
+function updateSizeOnScreen() {
+    sizeEl.innerText = size
+}
+
+increaseBtn.addEventListener('click', () => {
+    size += 5
+    if(size > 50) {
+        size = 50
+    }
+
+    updateSizeOnScreen()
+})
 
 
-drawCircle(100, 200)
-drawLine(300, 300, 300, 500)
+decreaseBtn.addEventListener('click', () => {
+    size -= 5
+    if(size < 5) {
+        size = 5
+    }
+
+    updateSizeOnScreen()
+})
+
+
+
+colorEl.addEventListener('change', (e) => color =e.target.value)
+
+
+clearEl.addEventListener('click', () => ctx.clearRect(0,0, canvas.width, canvas.height))
