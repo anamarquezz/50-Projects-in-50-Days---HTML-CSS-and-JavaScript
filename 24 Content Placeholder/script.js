9184a0d4e406cf34924060a01b4fbcea3f39cbab
AnaMarquez <anamarquez.edu@hotmail.com>
2022-02-19 23:52:57 -0800

    update

---
+++ 24 Content Placeholder/script.js
@@ -0,0 +1,53 @@
+
+Skip to content
+Pull requests
+Issues
+Marketplace
+Explore
+@anamarquezz
+PacktPublishing /
+50-Projects-In-50-Days---HTML-CSS-JavaScript
+Public
+
+Code
+Issues 1
+Pull requests
+Actions
+Wiki
+Security
+
+    Insights
+
+50-Projects-In-50-Days---HTML-CSS-JavaScript/content-placeholder/script.js /
+@kavyashreeshah
+kavyashreeshah Updated
+Latest commit cb7b816 24 days ago
+History
+1 contributor
+26 lines (22 sloc) 1.1 KB
+const header = document.getElementById('header')
+const title = document.getElementById('title')
+const excerpt = document.getElementById('excerpt')
+const profile_img = document.getElementById('profile_img')
+const name = document.getElementById('name')
+const date = document.getElementById('date')
+
+const animated_bgs = document.querySelectorAll('.animated-bg')
+const animated_bg_texts = document.querySelectorAll('.animated-bg-text')
+
+setTimeout(getData, 2500)
+
+function getData() {
+  header.innerHTML =
+    '<img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80" alt="" />'
+  title.innerHTML = 'Lorem ipsum dolor sit amet'
+  excerpt.innerHTML =
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore perferendis'
+  profile_img.innerHTML =
+    '<img src="https://randomuser.me/api/portraits/men/45.jpg" alt="" />'
+  name.innerHTML = 'John Doe'
+  date.innerHTML = 'Oct 08, 2020'
+
+  animated_bgs.forEach((bg) => bg.classList.remove('animated-bg'))
+  animated_bg_texts.forEach((bg) => bg.classList.remove('animated-bg-text'))
+}
