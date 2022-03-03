165525bb0c62a4e9e11a8a0bb54478630270dcff
AnaMarquez <anamarquez.edu@hotmail.com>
2022-03-02 22:22:46 -0800

    passwordGenerator

---
+++ 31 Password Generator/script.js
@@ -0,0 +1,53 @@
+const resultEl = document.getElementById('result')
+const lengthEl = document.getElementById('length')
+const upperCaseEl = document.getElementById('uppercase')
+const lowerCaseEl = document.getElementById('lowercase')
+const numbersEl = document.getElementById('numbers')
+const symbolsEl = document.getElementById('symbols')
+const generateEl = document.getElementById('generate')
+const clipboardEl = document.getElementById('clipboard')
+
+
+const randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbol
+}
+
+generateEl.addEventListener('click', () => {
+    const length = + lengthEl.value;
+    const hasLower = lowerCaseEl.checked;
+
+    const hasUpper = upperCaseEl.checked;
+    const hasNumber = numbersEl.checked;
+    const hasSymbol = symbolsEl.checked;
+
+    resultEl.innerHTML = generatePassword(hasLower, hasUpper, hasNumber,hasSymbol, length)
+
+
+})
+
+
+function generatePassword(lower, upper, number, symbol, length){
+    let generatePassword = '';
+    const typesCount = lower + upper + symbol;
+
+}
+function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+}
+
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+}
+
+function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
+}
+
+function getRandomSymbol() {
+    const symbols = '!@#$%^&*()=<>/,.'
+    return symbols[Math.floor(Math.random() * symbols.length)]
+}
+console.log(getRandomSymbol())
