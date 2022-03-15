b0a27c111c474f4f5e9576ffa1dd41d214dee43e
AnaMarquez <anamarquez.edu@hotmail.com>
2022-03-14 22:57:29 -0700

    update password Generator

--- 31 Password Generator/script.js
+++ 31 Password Generator/script.js
@@ -2,11 +2,10 @@ const resultEl = document.getElementById('result')
 const lengthEl = document.getElementById('length')
 const upperCaseEl = document.getElementById('uppercase')
 const lowerCaseEl = document.getElementById('lowercase')
-const numbersEl = document.getElementById('numbers')
-const symbolsEl = document.getElementById('symbols')
+const numberCaseEl = document.getElementById('numbers')
+const symbolCaseEl  = document.getElementById('symbols')
 const generateEl = document.getElementById('generate')
 const clipboardEl = document.getElementById('clipboard')
-
 
 const randomFunc = {
     lower: getRandomLower,
@@ -16,38 +15,32 @@ const randomFunc = {
 }
 
 generateEl.addEventListener('click', () => {
-    const length = + lengthEl.value;
+    const length = + lengthEl.value;  
     const hasLower = lowerCaseEl.checked;
-
     const hasUpper = upperCaseEl.checked;
-    const hasNumber = numbersEl.checked;
-    const hasSymbol = symbolsEl.checked;
+    const hasNumber = numberCaseEl.checked;
+    const hasSymbol = symbolCaseEl.checked;
 
-    resultEl.innerHTML = generatePassword(hasLower, hasUpper, hasNumber,hasSymbol, length)
+    resultEl.innerHTML = generatePassword(hasLower,hasUpper,hasUpper, hasNumber, hasSymbol, length);
 
+});
 
-})
-
-
-function generatePassword(lower, upper, number, symbol, length){
-    let generatePassword = '';
-    const typesCount = lower + upper + symbol;
-
+function getGeneratePassword() {
+    let generatePassword = ''
+    const typesCount= lower + '' + upper + '' +symbol+ console.log(typesCount)
 }
 function getRandomLower() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97 );
 }
 
 function getRandomUpper() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65); 
 }
-
-function getRandomNumber() {
+function getRandomNumber () {
     return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
 }
 
-function getRandomSymbol() {
-    const symbols = '!@#$%^&*()=<>/,.'
-    return symbols[Math.floor(Math.random() * symbols.length)]
+function getRandomSymbol () {
+    const symbols = '!@#$%^&*()[]=<>,./'
+    return symbols[Math.floor(Math.random() * symbols.length)];
 }
-console.log(getRandomSymbol())
